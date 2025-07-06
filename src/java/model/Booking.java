be97477f123d7b1e6547ce8af666a9ccef3ee321
PJMinh2812 <parkjeaminh2812@gmail.com>
2025-07-07 04:21:57 +0700

    Revert "template"
    This reverts commit 2fb7e8e455ee26e97a38ac5816dd20cbcd2bc468.

--- src/java/model/Booking.java
+++
@@ -1,9 +0,0 @@
-package model;
-import java.sql.Timestamp;
-
-public class Booking {
-    private int id, customerId, restaurantId;
-    private Timestamp bookingTime;
-    private boolean preOrder;
-    // Constructors, Getters, Setters
-}
--- src/java/model/Food.java
+++
@@ -1,8 +0,0 @@
-package model;
-
-public class Food {
-    private int id;
-    private String name, description;
-    private double price;
-    // Constructors, Getters, Setters
-}
