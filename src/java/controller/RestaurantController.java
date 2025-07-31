ab36e382aba5790b2708a35efe2d61ae983cfdb3
HuuTriet <162295740+HuuTriet@users.noreply.github.com>
2025-07-31 08:09:53 +0700

    Update RestaurantController.java

--- src/java/controller/RestaurantController.java
+++ src/java/controller/RestaurantController.java
@@ -1,11 +1,9 @@
 package Controller;
 
-import Dao.CustomerDao;
-import Dao.TasteDao;
-import Dao.UserDao;
-import Model.Constant.Gender;
-import Model.Customer;
-import Model.Taste;
+import Dao.RestaurantDao;
+import Dao.RestaurantTableDao;
+import Model.Restaurant;
+import Model.RestaurantTable;
 import Model.User;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -14,57 +12,138 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import java.time.LocalDate;
-import java.util.Arrays;
+import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
-public class  CustomerController {
-    @WebServlet("/customer/profile")
-    public static class ProfileServlet extends HttpServlet {
+public class RestaurantController {
+    @WebServlet("/restaurant/profile")
+    public static class RestaurantProfileServlet extends HttpServlet {
         @Override
         protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
             User user = (User) req.getSession().getAttribute("user");
-            Customer customer = new CustomerDao().getById(user.getId());
-            req.setAttribute("customer", customer);
-            req.getRequestDispatcher("/views/customer/profile.jsp").forward(req, resp);
+            Restaurant restaurant = new RestaurantDao().getById(user.getId());
+            req.setAttribute("restaurant", restaurant);
+            req.getRequestDispatcher("/views/restaurant/profile.jsp").forward(req, resp);
         }
 
         @Override
         protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-            CustomerDao customerDao = new CustomerDao();
-            String firstName = req.getParameter("firstName");
-            String lastName = req.getParameter("lastName");
-            LocalDate dateOfBirth = LocalDate.parse(req.getParameter("dateOfBirth"));
-            Gender gender = Gender.valueOf(req.getParameter("gender"));
+            RestaurantDao restaurantDao = new RestaurantDao();
+            String mapEmbedUrl = req.getParameter("mapEmbedUrl");
+            LocalTime openTime = LocalTime.parse(req.getParameter("openTime"));
+            LocalTime closeTime = LocalTime.parse(req.getParameter("closeTime"));
             User user = (User) req.getSession().getAttribute("user");
-            Customer customer = customerDao.getById(user.getId());
-            customer.setFirstName(firstName);
-            customer.setLastName(lastName);
-            customer.setDateOfBirth(dateOfBirth);
-            customer.setGender(gender);
-            customerDao.update(customer);
-            req.getSession().setAttribute("flash_success", "Cập nhật người dùng thành công.");
-            resp.sendRedirect(req.getContextPath() + "/customer/profile");
+            Restaurant restaurant = restaurantDao.getById(user.getId());
+            restaurant.setMapEmbedUrl(mapEmbedUrl);
+            restaurant.setOpenTime(openTime);
+            restaurant.setCloseTime(closeTime);
+            restaurantDao.update(restaurant);
+            req.getSession().setAttribute("flash_success", "Cập nhật nhà hàng thành công.");
+            resp.sendRedirect(req.getContextPath() + "/restaurant/profile");
         }
     }
 
-    @WebServlet("/customer/tastes")
-    public static class TastesServlet extends HttpServlet {
+    @WebServlet("/restaurant/tables")
+    public static class RestaurantTableServlet extends HttpServlet{
+        @Override
+        protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+            User user = (User) req.getSession().getAttribute("user");
+            RestaurantDao restaurantDao = new RestaurantDao();
+            Restaurant restaurant = restaurantDao.getRestaurantWithTables(user.getId());
+            req.getSession().setAttribute("tables", restaurant.getRestaurantTables());
+            req.getRequestDispatcher("/views/restaurant/tables.jsp").forward(req, resp);
+        }
+
         @Override
         protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-            TasteDao tasteDao = new TasteDao();
-            UserDao userDao = new UserDao();
             User user = (User) req.getSession().getAttribute("user");
-            user = userDao.getById(user.getId());
-            List<Long> tastesIds = Optional.ofNullable(req.getParameterValues("tastesIds")).stream().flatMap(Arrays::stream)
-                    .map(Long::parseLong)
-                    .toList();
-            List<Taste> tastes = tasteDao.getByIds(tastesIds);
-            user.setFavoriteTastes(tastes);
-            userDao.update(user);
-            req.getSession().setAttribute("user", user);
-            resp.sendRedirect(req.getHeader("referer"));
+            RestaurantDao restaurantDao = new RestaurantDao();
+            RestaurantTableDao restaurantTableDao = new RestaurantTableDao();
+            Restaurant restaurant = restaurantDao.getById(user.getId());
+            int from = Integer.parseInt(req.getParameter("from"));
+            if (req.getParameter("to").isEmpty()){
+                List<Integer> numbers = new ArrayList<>();
+                numbers.add(from);
+                if (restaurantTableDao.existsAnyNumberInList(numbers, restaurant)) {
+                    req.getSession().setAttribute("flash_error", "Số bàn đã bị trùng.");
+                    resp.sendRedirect(req.getContextPath() + "/restaurant/tables");
+                    return;
+                }
+                RestaurantTable restaurantTable = new RestaurantTable(from, restaurant, true, false);
+                restaurantTableDao.save(restaurantTable);
+            } else {
+               int to = Integer.parseInt(req.getParameter("to"));
+                List<Integer> list = IntStream.rangeClosed(from, to)
+                        .boxed()
+                        .toList();
+                if (restaurantTableDao.existsAnyNumberInList(list, restaurant)) {
+                    req.getSession().setAttribute("flash_error", "Số bàn đã bị trùng.");
+                    resp.sendRedirect(req.getContextPath() + "/restaurant/tables");
+                    return;
+                }
+                List<RestaurantTable> tables = new ArrayList<>();
+                for (int i = from; i <= to; i++) {
+                    tables.add(new RestaurantTable(i, restaurant, true, false));
+                }
+                restaurantTableDao.saveAll(tables);
+            }
+            req.getSession().setAttribute("flash_success", "Thêm mới thành công.");
+            resp.sendRedirect(req.getContextPath() + "/restaurant/tables");
+        }
+    }
+    @WebServlet("/restaurant/edit-table")
+    public static class RestaurantEditTable extends HttpServlet{
+        @Override
+        protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+            User user = (User) req.getSession().getAttribute("user");
+            RestaurantDao restaurantDao = new RestaurantDao();
+            RestaurantTableDao restaurantTableDao = new RestaurantTableDao();
+            Restaurant restaurant = restaurantDao.getRestaurantWithTables(user.getId());
+            long id = Long.parseLong(req.getParameter("id"));
+            RestaurantTable restaurantTable = restaurantTableDao.getTableWithRestaurant(restaurant, id);
+            if (restaurantTable == null){
+                req.getSession().setAttribute("flash_error", "Không tìm thấy bàn này.");
+                return;
+            }
+            int number = Integer.parseInt(req.getParameter("number"));
+            List<Integer> numbers = new ArrayList<>();
+            numbers.add(number);
+            if (number != restaurantTable.getNumber() && restaurantTableDao.existsAnyNumberInList(numbers, restaurant)) {
+                req.getSession().setAttribute("flash_error", "Số bàn đã bị trùng.");
+                resp.sendRedirect(req.getContextPath() + "/restaurant/tables");
+                return;
+            }
+            boolean isHavingCustomer = Boolean.parseBoolean(req.getParameter("havingCustomer"));
+            boolean isAvailable = Boolean.parseBoolean(req.getParameter("available"));
+            restaurantTable.setNumber(number);
+            restaurantTable.setHavingCustomer(isHavingCustomer);
+            restaurantTable.setAvailable(isAvailable);
+            restaurantTableDao.update(restaurantTable);
+            req.getSession().setAttribute("flash_success", "Cập nhật thành công.");
+            resp.sendRedirect(req.getContextPath() + "/restaurant/tables");
+        }
+    }
+    @WebServlet("/restaurant/change-having-customer")
+    public static class RestaurantChangeHavingCustomer extends HttpServlet{
+        @Override
+        protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+            User user = (User) req.getSession().getAttribute("user");
+            RestaurantDao restaurantDao = new RestaurantDao();
+            RestaurantTableDao restaurantTableDao = new RestaurantTableDao();
+            Restaurant restaurant = restaurantDao.getById(user.getId());
+            long id = Long.parseLong(req.getParameter("id"));
+            RestaurantTable restaurantTable = restaurantTableDao.getTableWithRestaurant(restaurant, id);
+            if (restaurantTable == null){
+                req.getSession().setAttribute("flash_error", "Không tìm thấy bàn này.");
+                return;
+            }
+            restaurantTable.setHavingCustomer(!restaurantTable.isHavingCustomer());
+            restaurantTableDao.update(restaurantTable);
+            req.getSession().setAttribute("flash_success", "Cập nhật thành công.");
+            resp.sendRedirect(req.getContextPath() + "/restaurant/tables");
         }
     }
 }
